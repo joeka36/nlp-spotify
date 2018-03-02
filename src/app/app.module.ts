60c2c25153062a2c07d047ac72dec8ca7f3fac84
Joe Akanesuvan <kantapon95@gmail.com>
2018-03-01 22:21:41 -0800

    Initial commit, set up landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { RecordingComponent } from './recording/recording.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    RecordingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
